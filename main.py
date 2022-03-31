a340b1c36d6811fa781165c7577d4252f7f470ff
Viktor Lumiste <viktor.lumiste@khk.ee>
2022-03-31 10:01:57 +0300

    Mina, kui kasutaja, saan sisestada riigi, et saada tagasi riigi käibemaksu määru [delivered #181737233]

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ hind = float(input("Sisestage 1 kauba hind: "))
 kogus = int(input("Sisesta kogus: "))
 riik = input("Sisestage riigi kood: ").upper()
 koguhind = hind * kogus
+def kaibemaks(riigikood):
+    if riigikood == "AL":
+        return 4
+    elif riigikood == "CA":
+        return 8.25
+    elif riigikood == "TX":
+        return 6.25
+    elif riigikood == "NV":
+        return 6
+    elif riigikood == "UT":
+        return 6.85
+    else:
+        return 0
 print("See on teie tekst: " + tekst)
 print("Ühe kauba hind on: " + str(hind) )
 print("Teie kogus on: " + str(kogus) )
 print("Kogu kauba hind on : " + str(koguhind))
 print("Teie riigi kood on" + str(riik))
+print(riik, "maks on", kaibemaks(riik), "%")
