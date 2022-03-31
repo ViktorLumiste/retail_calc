7c633c29aecded3d246d5426b0b9b2a134defd90
Viktor Lumiste <viktor.lumiste@khk.ee>
2022-03-31 09:57:18 +0300

    Mina, kui kasutaja, saan sisestada riigi, et saada tagasi riigi [delivered #181737229]

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 tekst = str(input("Sisesta tekst: "))
 hind = float(input("Sisestage 1 kauba hind: "))
 kogus = int(input("Sisesta kogus: "))
+riik = input("Sisestage riigi kood: ").upper()
 koguhind = hind * kogus
 print("See on teie tekst: " + tekst)
 print("Ühe kauba hind on: " + str(hind) )
 print("Teie kogus on: " + str(kogus) )
 print("Kogu kauba hind on : " + str(koguhind))
+print("Teie riigi kood on" + str(riik))
